42ce542ce25b8d3a50b8d928d53901fed68edcd1
Ananya Phadke <phadkeananya@gmail.com>
2018-07-10 20:23:14 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -20,13 +20,13 @@ def last_student_by_index
 end
 
 def first_student_by_method
-  room.first
+  STUDENT_NAMES.first
 end
 
 def last_student_by_method
-  room.last
+  STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+  STUDENT_NAMES[0,1,2]
 end
